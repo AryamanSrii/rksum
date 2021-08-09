2f7218cca2abdbd59a9c394c675262f95e47ae69
Aryaman Srivastava <aryamansrivastava2006@gmail.com>
2021-08-09 09:18:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+import nltk
+nltk.download("punkt")
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+print("enter text to summarize")
+input1 = input()
+text = f"""{input1}"""
+
+stopWords = set(stopwords.words("english"))
+words = word_tokenize(text)
+freqTable = dict()
+for word in words:
+	word = word.lower()
+	if word in stopWords:
+		continue
+	if word in freqTable:
+		freqTable[word] += 1
+	else:
+		freqTable[word] = 1
+sentences = sent_tokenize(text)
+sentenceValue = dict()
+
+for sentence in sentences:
+	for word, freq in freqTable.items():
+		if word in sentence.lower():
+			if sentence in sentenceValue:
+				sentenceValue[sentence] += freq
+			else:
+				sentenceValue[sentence] = freq
+
+sumValues = 0
+for sentence in sentenceValue:
+	sumValues += sentenceValue[sentence]
+average = int(sumValues / len(sentenceValue))
+summary = ''
+for sentence in sentences:
+	if (sentence in sentenceValue) and (sentenceValue[sentence] > (1 * average)):
+		summary += " " + sentence
+print(summary) 
+res = len(text.split())
+sum = len(summary.split())
+quotient = sum/res
+percent = quotient * 100
+f = round(percent, 2)
+print ("\n \nThe Number of Word in given text were " +  str(res) + " and After summarization are " + str(sum) + f" Reduced by {f}%")
+
+
+
